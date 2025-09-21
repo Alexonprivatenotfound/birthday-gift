295f0ae8f66ce9f2a274f0af2de8474f4858108e
Alexonprivatenotfound <aulettaalessandro80@gmail.com>
2025-09-21 13:02:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function addCandle(left, top) {
     // ❌ prevent adding more than 17
-    if (candles.filter(c => !c.classList.contains("out")).length >= 17) return;
+    if (candles.filter(c => !c.classList.contains("out")).length >= 54) return;
 
     const candle = document.createElement("div");
     candle.className = "candle";
@@ -45,11 +45,13 @@ document.addEventListener("DOMContentLoaded", function () {
     let average = sum / bufferLength;
 
     // 🔧 Increase mic sensitivity by lowering threshold
-    return average > 25; // was 40, now more sensitive
+    return average > 50; // was 40, now more sensitive
   }
 
   function blowOutCandles() {
     if (!isBlowing()) return;
+
+    if (candles.lenght < 54) return;
 
     let blownOut = 0;
 
